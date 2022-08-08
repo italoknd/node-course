f00d547ff7b3fea44dbd7f6dcbe0209211180c07
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2022-08-08 10:23:37 -0400

    aprendendo sobre o novo método de importação e exportação

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-const calc = require('./modules/calculadora')
+//maneira mais recente de importação
+import {
+  somar,
+  subtrair,
+  outrasOperacoes,
+  calcularPotencia,
+  userInfos
+} from './modules/calculadora.js'
+//<<<< importante colocar a extensão js (?)
 
-console.log(calc.a.somar(2, 2))
-console.log(calc.a.subtrair(15, 6.5))
-console.log(calc.b.mult(2, 5))
-console.log(calc.b.divisao(2, 2))
+console.log(somar(2, 5))
+console.log(subtrair(2, 5))
+console.log(outrasOperacoes.mult(2, 5))
+console.log(outrasOperacoes.divisao(2, 5))
+
+console.log(calcularPotencia(5, 2))
+
+console.log(
+  (userInfos.nome = 'Italo'),
+  (userInfos.sobrenome = 'Pedroza'),
+  (userInfos.idade = 22),
+  (userInfos.sexo = 'M')
+)
+
+//maneira comum de importação (commonjs)
+// const calc = require('./modules/calculadora')
+
+// console.log(calc.a.somar(2, 2))
+// console.log(calc.a.subtrair(15, 6.5))
+// console.log(calc.b.mult(2, 5))
+// console.log(calc.b.divisao(2, 2))
--- modules/calculadora.js
+++ modules/calculadora.js
@@ -15,4 +15,20 @@ const outrasOperacoes = {
   }
 }
 
-module.exports = { a: { somar, subtrair }, b: outrasOperacoes }
+//maneira comum de exportação (commomjs)
+//module.exports = { a: { somar, subtrair }, b: outrasOperacoes }
+
+//maneira mais recente de exportação
+export { somar, subtrair, outrasOperacoes }
+
+/************** OUTRA MANEIRA DE EXPORTAR MÓDULOS ******************/
+export const calcularPotencia = function (a, b) {
+  return a ** b
+}
+
+export const userInfos = {
+  nome: '',
+  sobrenome: '',
+  idade: null,
+  sexo: ''
+}
