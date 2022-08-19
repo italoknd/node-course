f283fca84da2900ca620c70cbccc18cd8e41670d
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2022-08-19 11:51:59 -0400

    aprendendo um pouco sobre express

--- express/express.js
+++ express/express.js
@@ -1,2 +1,12 @@
 const express = require('express')
 const app = express() // a variavel app vai receber tudo que tem dentro do modulo express
+
+const PORT = process.env.PORT || 3000 
+
+app.get('/', (req, res)=>{
+  res.sendFile(__dirname + './public/index.html')
+})
+
+app.listen(PORT, ()=>{
+  console.log(`Servidor rodando em http://localhost:${PORT}`)
+});
